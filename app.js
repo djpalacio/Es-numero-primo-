b5db0af0ea821fb687e1bd3292a04cc8c07509d2
Daniel <dj_palacio_10@hotmail.com>
2024-08-06 21:29:11 -0500

    primer commit de programa en javascript que calcula si un número es primo o no

---
+++ app.js
@@ -0,0 +1,36 @@
+let numero = document.querySelector(".numero")
+let boton = document.querySelector(".enviar")
+let parrafo = document.querySelector(".resultado")
+
+
+boton.addEventListener("click",esNumPrimo)
+
+
+function esNumPrimo(){
+    let num = Number(numero.value)
+    if (num == 1){
+        parrafo.textContent = "Número no válido"
+    }
+    else{
+        esPrimo = true
+        for(let i = 2; i < num ; i++){
+            if (num % i == 0){
+                esPrimo = false
+                break
+            }
+        }
+        if (esPrimo == true){
+            parrafo.textContent = "El número es primo"
+        }
+       
+      else if (esPrimo== false){
+        parrafo.textContent = "El número no es primo"
+
+      }
+       
+      
+
+    }
+
+}
+
